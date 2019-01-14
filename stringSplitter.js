ba7156198cd149c53dee9ea221506fdac11502ff
Stephen Anderson <anderssj@usc.edu>
2019-01-14 11:17:00 -0800

    stringSplitter

---
+++ stringSplitter.js
@@ -0,0 +1,12 @@
+function stringSplitter(str, splitter) {
+
+  if(str.indexOf(splitter) === -1){
+      return [str];
+  }else{
+      let temp = str.indexOf(splitter);
+      let tempString = str.slice(0,temp);
+      return [tempString, ...stringSplitter(str.slice(temp+1),splitter) ]
+  }
+}
+
+console.log(stringSplitter('Steve Is sdsfsd', ' '));
