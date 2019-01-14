cf1e6a02e3174afed2a1a9a4f6b1f34414dda147
Stephen Anderson <anderssj@usc.edu>
2019-01-14 11:21:25 -0800

    s

--- stringSplitter.js
+++ stringSplitter.js
@@ -2,7 +2,8 @@ function stringSplitter(str, splitter) {
 
   if(str.indexOf(splitter) === -1){
       return [str];
-  }else{
+  }
+  else {
       let temp = str.indexOf(splitter);
       let tempString = str.slice(0,temp);
       return [tempString, ...stringSplitter(str.slice(temp+1),splitter) ]
