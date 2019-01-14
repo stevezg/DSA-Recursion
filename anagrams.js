27ea6f1759f7e7361a536fa7bd94c9f060c1041f
seankrummel <sean.krummel@gmail.com>
2019-01-14 14:48:55 -0600

    anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,8 +1,23 @@
+'use strict';
+// east
 function anagrams (word) {
-  _ _ _
-  let firstletter = word[0]
-  // base case : word is 1 letter
-  if(word.length === 1){
-    return ""
+  // get a letter from word and use as prefix
+  const results = [];
+  function anagramsHelper(prefix, word1) {
+    // base case : word is 1 letter
+    if(word1.length === 1){
+      results.push(prefix + word1);
+      return word1[0];
+    }
+    word1.split('').forEach((pre, index) => {
+      let firstPartOfWord = word1.slice(0,index);
+      let secondPartOfWord = word1.slice(index + 1);
+      const result = anagramsHelper(prefix + pre, firstPartOfWord + secondPartOfWord);
+      return result;
+    });
   }
+  anagramsHelper('', word);
+  return results;
 }
+
+console.log(anagrams('easts').length);
