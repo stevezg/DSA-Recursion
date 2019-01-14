daaec312a0fc0c1aebb6ce8803b8a736c2161ec3
Stephen Anderson <anderssj@usc.edu>
2019-01-14 12:22:17 -0800

    anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,8 +1,20 @@
 function anagrams (word) {
-  _ _ _
-  let firstletter = word[0]
-  // base case : word is 1 letter
-  if(word.length === 1){
-    return ""
-  }
+
+  let results = {};
+
+  let combos = function (buildCombo, feed) {
+    if (!feed.length) {
+      results[buildCombo] = '';
+      return;
+    }
+    for (let i = 0; i < feed.length; i++) {
+      combos(buildCombo + feed.charAt(i), feed.slice(0, i) + feed.slice(i + 1));
+    }
+  };
+
+  combos('', word);
+  return Object.keys(results);
+
 }
+
+console.log(anagrams('steve'));
